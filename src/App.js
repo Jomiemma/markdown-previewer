3c1fd6d2d04f3ad6e263ac33a95270e1dfd5c2c3
Emmanuel Enujeko <enujekojomiloju@gmail.com>
2025-06-27 18:15:16 +0100

    updated files with previewer code

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { marked } from 'marked';
+import './index.css';
+
+function App() {
+  const [markdown, setMarkdown] = React.useState(`# Welcome to my Markdown Previewer!
+
+## This is a subheading
+[I'm a link](https://example.com)
+Here is some \`inline code\`.
+
+\`\`\`
+This is a code block
+function helloWorld() {
+  console.log("Hello!");
+}
+\`\`\`
+
+- List item 1
+- List item 2
+
+> This is a blockquote
+
+**This is bold text**
+
+![React Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`);
+
+  return (
+    <div id="container">
+      <textarea
+        id="editor"
+        value={markdown}
+        onChange={(e) => setMarkdown(e.target.value)}
+      />
+      <div
+        id="preview"
+        dangerouslySetInnerHTML={{
+          __html: marked.parse(markdown, { breaks: true }),
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default App; // 🔵 Required so index.js can load this
